3d278b08af9b75cee4927dcf5f70bc70cfbbb578
takara1356 <goblin7283@gmail.com>
2022-06-30 16:49:40 +0900

    login_datesテーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,14 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "login_dates", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "login_date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_login_dates_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.bigint "discord_id", null: false
     t.string "discord_name", null: false
@@ -22,4 +30,5 @@ ActiveRecord::Schema.define(version: 0) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "login_dates", "users"
 end
